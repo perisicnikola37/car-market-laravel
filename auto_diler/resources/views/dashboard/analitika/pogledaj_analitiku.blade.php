e7a5c863eb04e02e4af0c461de8cdbdddc950970
Nikola Perisic <5dzoni5@gmail.com>
2022-06-17 15:03:47 +0100

    Changed ColumnSize for Analytics view

--- auto_diler/resources/views/dashboard/analitika/pogledaj_analitiku.blade.php
+++ auto_diler/resources/views/dashboard/analitika/pogledaj_analitiku.blade.php
@@ -12,7 +12,7 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.1/chart.min.js" integrity="sha512-QSkVNOCYLtj73J4hbmVoOV6KVZuMluZlioC+trLpewV8qMjsWqlIQvkn1KGX2StWvPMdWGBqim1xlC8krl1EKQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-<div class="col-sm-7">
+<div class="col-sm-3">
 	<canvas id="myChart"></canvas>
 </div>
 
