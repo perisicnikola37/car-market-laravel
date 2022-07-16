a0c7d58a0b29613ecec863efd091f28b120a2a91
Nikola Perisic <5dzoni5@gmail.com>
2022-07-16 14:13:05 +0200

    Improve routes

--- routes/web.php
+++ routes/web.php
@@ -1,63 +1,51 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-
+Route::auth();
 
 Route::group(['middleware' => 'isadmin'], function() {
-
 Route::resource('/analitika', App\Http\Controllers\AnalitikaController::class);
-
 // Korisnici Dashboard
 Route::get('/svi-korisnici', [App\Http\Controllers\KorisniciDashboardController::class, 'index'])->name('svi-korisnici');
-
 });
 
-
+Route::controller(DashboardController::class)->group(function() {
 // Redirect na /naslovna
-Route::get('/', 'App\Http\Controllers\DashboardController@redirect');
-
+Route::get('/', [DashboardController::class, 'redirect']);
 Route::get('/naslovna', [App\Http\Controllers\DashboardController::class, 'index'])->name('dashboard');
-
-Route::auth();
+Route::resource('/car', App\Http\Controllers\DashboardController::class);
+Route::get('/oglas/{slug}', [App\Http\Controllers\DashboardController::class, 'show'])->name('slug');
+});
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource('/dodaj-oglas', App\Http\Controllers\AdminController::class)->middleware('limit');
 
-Route::resource('/car', App\Http\Controllers\DashboardController::class);
 Route::resource('/profil', App\Http\Controllers\ProfileController::class);
 
 Route::get('/profil-podesavanja', [App\Http\Controllers\ProfileController::class, 'index2'])->name('profil-podesavanja');
 
-
-// Oglasi Dashboard
-Route::get('/svi-oglasi', [App\Http\Controllers\OglasiDashboardController::class, 'index'])->name('svi-oglasi');
-
-
-
+Route::controller(KorisniciDashboardController::class)->group(function() {
 // Za brisanje korisnika
 Route::delete('/izbriši-korisnika/{id}', [App\Http\Controllers\KorisniciDashboardController::class, 'destroy'])->name('izbriši-korisnika');
-
 // Za prikazivanje korisnika
 Route::get('/prikaži-korisnika/{id}', [App\Http\Controllers\KorisniciDashboardController::class, 'show'])->name('prikaži-korisnika');
+});
 
-
+Route::controller(OglasiDashboardController::class)->group(function() {
+// Oglasi Dashboard
+Route::get('/svi-oglasi', [App\Http\Controllers\OglasiDashboardController::class, 'index'])->name('svi-oglasi');
 Route::get('/izmijeni-oglas/{slug}', [App\Http\Controllers\OglasiDashboardController::class, 'edit'])->name('izmijeni-oglas');
-
-
 // Prije bez slug-a
 // Route::get('/izmijeni-oglas/{id}', [App\Http\Controllers\OglasiDashboardController::class, 'edit'])->name('izmijeni-oglas');
-
 Route::patch('/izmijeni-oglas/{id}/update', [App\Http\Controllers\OglasiDashboardController::class, 'update'])->name('izmijeni-oglas-update');
-
-
 Route::delete('/izmijeni-oglas/{id}', [App\Http\Controllers\OglasiDashboardController::class, 'destroy'])->name('izmijeni-oglas-destroy');
-
-Route::get('/oglas/{slug}', [App\Http\Controllers\DashboardController::class, 'show'])->name('slug');
+});
 
 // Route za logout
-// U blade fajlu sam napisao u a button-u href="{{route('logout')}}
-Route::get('/logout', 'App\Http\Controllers\Auth\LoginController@logout');
+Route::get('/logout', [LoginController::class, 'logout']);
 
 
